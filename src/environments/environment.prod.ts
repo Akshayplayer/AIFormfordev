2646c8ebd49c23ebf52a6bcd008a81c004b8c55d
Akshayplayer <akshayku.bothra@gmail.com>
2025-08-21 17:08:38 +0530

    deployed backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // baseApiUrl: 'https://backendpoc-f5eugxbhcrd9akfu.canadacentral-01.azurewebsites.net/api/Controllerwebapp'
-  baseApiUrl: 'https://localhost:7111/api/Controllerwebapp'
+  baseApiUrl: 'https://backendpoc-f5eugxbhcrd9akfu.canadacentral-01.azurewebsites.net/api/Controllerwebapp'
+  // baseApiUrl: 'https://localhost:7111/api/Controllerwebapp'
 };
